f5d453e07efffde9c8bf39e29c1ba835fa7b1c9a
Bruna Rupp <brunaafrupp@outlook.com>
2019-12-02 20:39:58 -0300

    Meu primeiro projeto git.

---
+++ Meu projeto git/Meu projeto git/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meu_projeto_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola mundo!");
+
+            Console.WriteLine("Meu primeiro projeto git");
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
