a8994bf471c2405b78ba432444979a9198f80c3f
Bruna Rupp <brunaafrupp@outlook.com>
2019-12-02 22:09:03 -0300

    Exercicio com list Carros.

---
+++ ListCarros/ListCarros/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListCarros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listaCarros = new List<string>
+            {
+                "Vectra",
+                "Polo",
+                "Sandero",
+                "Chevette",
+                "Corvete"
+            };
+
+            listaCarros.ForEach(x => Console.WriteLine(x));
+
+            Console.ReadKey(true);
+        }
+    }
+}
